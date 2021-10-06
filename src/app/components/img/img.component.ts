1bd7839661d7bcd549b5ec6d45c0bc448f75eef2
Carlos Eduardo Villanueva Altuna <carlos.villanueva.altuna@gmail.com>
2021-10-06 14:46:22 -0300

    Angular intermediate - components - new store

---
+++ src/app/components/img/img.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-img',
+  templateUrl: './img.component.html',
+  styleUrls: ['./img.component.scss'],
+})
+export class ImgComponent implements OnInit {
+  @Input() img: string = '';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
