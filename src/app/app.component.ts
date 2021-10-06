ec0569430814085d5b1cec76be34e1bd368464b3
Carlos Eduardo Villanueva Altuna <carlos.villanueva.altuna@gmail.com>
2021-10-06 20:39:33 -0300

    Angular intermediate - Components - New Store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'new-store';
+  imgParent = '';
+
+  // el evento del componente hijo envia la url de la imagen
+  onLoaded(img: string) {
+    console.info(`Log from Parent ${img}`);
+  }
 }
--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-img',
@@ -7,8 +7,23 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ImgComponent implements OnInit {
   @Input() img: string = '';
+  imageDefault: string = './assets/images/bike.jpg';
+
+  // enviando un evento al componente padre
+  // EventEmitter envia strings
+  @Output() loaded = new EventEmitter<string>();
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  imgError() {
+    this.img = this.imageDefault;
+  }
+
+  imgLoaded() {
+    console.info('Log from child');
+    // emitimos el evento
+    this.loaded.emit(this.img);
+  }
 }
