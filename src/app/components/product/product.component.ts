8a9be6836473fec4f6453cdb8040ea242b96aba9
Carlos Eduardo Villanueva Altuna <carlos.villanueva.altuna@gmail.com>
2021-10-06 21:53:14 -0300

    Angular intermediate - components - new store

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Product } from '../../../models/product.model';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  styleUrls: ['./product.component.scss'],
 })
 export class ProductComponent implements OnInit {
+  @Input() product: Product = {
+    // definimos el estado inicial
+    id: '',
+    name: '',
+    price: 0,
+    image: '',
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
