8201ff5be5ea05aca01a1f420f07f90aacb1d23f
Carlos Eduardo Villanueva Altuna <carlos.villanueva.altuna@gmail.com>
2021-10-12 10:11:05 -0300

    Angular intermediate - Components - New Store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 
-import { Product } from '../models/product.model';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,33 +9,6 @@ export class AppComponent {
   title = 'new-store';
   imgParent = '';
   showImg = true;
-
-  products: Product[] = [
-    {
-      id: '1',
-      name: 'EL mejor juguete',
-      price: 565,
-      image: './assets/images/toy.jpg',
-    },
-    {
-      id: '2',
-      name: 'Bicicleta casi nueva',
-      price: 356,
-      image: './assets/images/bike.jpg',
-    },
-    {
-      id: '3',
-      name: 'Colleción de albumnes',
-      price: 34,
-      image: './assets/images/album.jpg',
-    },
-    {
-      id: '4',
-      name: 'Mis libros',
-      price: 23,
-      image: './assets/images/books.jpg',
-    },
-  ];
 
   // el evento del componente hijo envia la url de la imagen
   onLoaded(img: string) {
--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -7,6 +7,7 @@ import {
   OnChanges,
   AfterViewInit,
   OnDestroy,
+  SimpleChanges,
 } from '@angular/core';
 
 @Component({
@@ -17,13 +18,21 @@ import {
 export class ImgComponent
   implements OnInit, OnChanges, AfterViewInit, OnDestroy
 {
-  @Input() img: string = '';
+  img: string = '';
+  @Input('img')
+  set changeImg(newImg: string) {
+    this.img = newImg;
+    console.info(`change just img => ${this.img}`);
+  }
+
+  @Input() alt: string = '';
+
   imageDefault: string = './assets/images/bike.jpg';
 
   //** ngDestroy & ngSetInput Sample */
-  counter: number = 0;
+  // counter: number = 0;
   // guardamos la referencia que retorna el metodo setInterval()
-  counterFn: number | undefined;
+  // counterFn: number | undefined;
   //** END ngDestroy & ngSetInput Sample */
 
   // enviando un evento al componente padre
@@ -42,15 +51,15 @@ export class ImgComponent
     //** ngDestroy & ngSetInput Sample */
     // corremos una tarea q se ejcuta por cada segundo y como es algo asincrono lo hacemos en el ngOnInit()
     // este evento setInterval() se queda activo aun cuando el componente app-img es destruido
-    this.counterFn = window.setInterval(() => {
-      this.counter += 1; // cada segundo sumamos 1 a counter
+    /* this.counterFn = window.setInterval(() => {
+      this.counter += 1;
       console.info(`run counter ${this.counter}`);
-    }, 1000);
+    }, 1000); */
     //** ngDestroy & ngSetInput Sample */
   }
 
   // se ejecuta antes del render()
-  ngOnChanges() {
+  ngOnChanges(changes: SimpleChanges) {
     console.info(`app-img component ngOnChanges imgValue=> ${this.img}`);
   }
 
@@ -62,7 +71,7 @@ export class ImgComponent
   ngOnDestroy() {
     console.info('app-img component ngOnDestroy');
     // limpiamos el evento del setInterval()
-    window.clearInterval(this.counterFn);
+    // window.clearInterval(this.counterFn);
   }
 
   imgError() {
